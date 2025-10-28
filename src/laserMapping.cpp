b2e62504d9c196df0aaed9267c06692a574ec9fa
DavideCarminati <davide.carminati@outlook.com>
2025-10-28 17:29:03 +0100

    Exposed R matrix tuning for EKF in the yaml config files

--- src/laserMapping.cpp
+++ src/laserMapping.cpp
@@ -89,6 +89,7 @@ string map_file_path, lid_topic, imu_topic;
 
 
 double MEAS_NOISE_COV_X = 0.1, MEAS_NOISE_COV_Y = 0.1, MEAS_NOISE_COV_Z = 0.1;
+bool use_ekf2_cov = false;
 double res_mean_last = 0.05, total_residual = 0.0;
 double last_timestamp_lidar = 0, last_timestamp_imu = -1.0;
 double gyr_cov = 0.1, acc_cov = 0.1, b_gyr_cov = 0.0001, b_acc_cov = 0.0001;
@@ -882,9 +883,16 @@ void h_share_model(state_ikfom &s, esekfom::dyn_share_datastruct<double> &ekfom_
     ekfom_data.h_x.block<3, 3>(effct_feat_num, 23) << -ROT_gps_imu * pos_crossmat;// - POS;
     ekfom_data.h_x.block<3, 3>(effct_feat_num, 26) << Eigen::Matrix3d::Identity();
     ekfom_data.R.block(0, 0, effct_feat_num, effct_feat_num) = LASER_POINT_COV * VectorXd::Ones(effct_feat_num).asDiagonal();
-    // ekfom_data.R.bottomRightCorner(3, 3) = Vector3d(MEAS_NOISE_COV_X, MEAS_NOISE_COV_Y, MEAS_NOISE_COV_Z).asDiagonal();    // The value is high because the odometry from px4 has LOW ACCURACY!
-    // Use the covariance on the position given directly by the PX4 EKF
-    ekfom_data.R.bottomRightCorner(3, 3) = Measures.px4_position_cov;
+    
+    if (use_ekf2_cov)
+    {
+        // Use the covariance on the position given directly by the PX4 EKF
+        ekfom_data.R.bottomRightCorner(3, 3) = Measures.px4_position_cov;
+    }
+    else
+    {
+        ekfom_data.R.bottomRightCorner(3, 3) = Vector3d(MEAS_NOISE_COV_X, MEAS_NOISE_COV_Y, MEAS_NOISE_COV_Z).asDiagonal();    // The value is high because the odometry from px4 has LOW ACCURACY!
+    }
     /* 
         Matrix h_v is not implemented.
     */
@@ -922,6 +930,7 @@ public:
         this->declare_parameter<double>("mapping.measurement_noise_covariance_x", 0.1);
         this->declare_parameter<double>("mapping.measurement_noise_covariance_y", 0.1);
         this->declare_parameter<double>("mapping.measurement_noise_covariance_z", 0.1);
+        this->declare_parameter<bool>("mapping.use_ekf2_covariance", false);
         this->declare_parameter<double>("preprocess.blind", 0.01);
         this->declare_parameter<int>("preprocess.lidar_type", AVIA);
         this->declare_parameter<int>("preprocess.scan_line", 16);
@@ -961,6 +970,7 @@ public:
         this->get_parameter_or<double>("mapping.measurement_noise_covariance_x", MEAS_NOISE_COV_X, 0.1);
         this->get_parameter_or<double>("mapping.measurement_noise_covariance_y", MEAS_NOISE_COV_Y, 0.1);
         this->get_parameter_or<double>("mapping.measurement_noise_covariance_z", MEAS_NOISE_COV_Z, 0.1);
+        this->get_parameter_or<bool>("mapping.use_ekf2_covariance", use_ekf2_cov, false);
         this->get_parameter_or<double>("preprocess.blind", p_pre->blind, 0.01);
         this->get_parameter_or<int>("preprocess.lidar_type", p_pre->lidar_type, AVIA);
         this->get_parameter_or<int>("preprocess.scan_line", p_pre->N_SCANS, 16);
